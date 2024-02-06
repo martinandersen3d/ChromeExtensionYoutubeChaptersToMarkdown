93b1210aa2fc405cb3df559f24916101595ebd80
Martin andersen <martinandersen3d@gmail.com>
2024-02-06 01:45:56 +0100

    popup works and markdown is in the box

--- src/background.js
+++ src/background.js
@@ -1,38 +1,22 @@
-chrome.runtime.onInstalled.addListener(function () {
-  console.log('Service Worker Installed');
-});
+chrome.runtime.onInstalled.addListener( function () {
+  // console.log( 'Service Worker Installed' );
+} );
 
-chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-  if (message.type === 'p-to-s') {
-    console.log('p-to-s: Received message in Service Worker:', message.data);
+chrome.runtime.onMessage.addListener( function ( message, sender, sendResponse ) {
+  if ( message.type === 'popup-to-background' ) {
 
     // Wait for the tab to be fully loaded before sending a message
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-      if (tabs[0] && tabs[0].id) {
-        chrome.tabs.sendMessage(tabs[0].id, { type: 's-to-c', data: 'Message from Service Worker to Content Script' }, function (response) {
-          if (chrome.runtime.lastError) {
+    chrome.tabs.query( { active: true, currentWindow: true }, function ( tabs ) {
+      if ( tabs[0] && tabs[0].id ) {
+        chrome.tabs.sendMessage( tabs[0].id, { type: 'background-to-content' }, function ( response ) {
+          // this line needs to be here i guess. I dont know why..
+          if ( chrome.runtime.lastError ) {
             // console.error(chrome.runtime.lastError.message);
-          } else {
-            console.log('Response from Content Script:', response);
           }
-        });
-      } else {
-        console.error('No active tab found.');
+        } );
       }
-    });
+    } );
   }
-
-  if (message.type === 'c-to-s') {
-    console.log('c-to-s: Received message in Service Worker:', message.data);
-      chrome.runtime.sendMessage({
-        msg: "something_completed", 
-        data: {
-            subject: "Loading",
-            content: "Just completed!"
-        }
-    });
-  }
-
 
 });
 
--- src/content-ym.js
+++ src/content-ym.js
@@ -1,28 +1,68 @@
 
+const chaptersArray = () => {
+  // EXTRACT TEXT FROM CHAPTERS --------------------------------------------
 
-console.log('CONTENT------------------------------')
+  // Selecting elements by the given selector
+  let nodeList = document.querySelectorAll("ytd-macro-markers-list-renderer ytd-macro-markers-list-item-renderer");
 
+  let output = []
+  for ( let detailsElement of nodeList ) {
+    // let detailsElement = document.getElementById('details');
 
-//   chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-//     if (message.type === 's-to-c') {
-//       console.log('Received message in Content Script:', message.data);
-//       chrome.runtime.sendMessage({ type: 'c-to-s', data: 'Message from Content Script to Service Worker' });
-//     }
-//   });
+    if ( nodeList ) {
+      // Extract text content from the h4 element
+      let title = detailsElement.firstElementChild.textContent.trim().split('\n')[0];
 
-chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-    if (message.type === 's-to-c') {
-      console.log('s-to-c: Received message in Content Script:', message.data);
-      // Handle the message from Service Worker if needed
-      // If a response is expected, you can send it asynchronously
-      chrome.runtime.sendMessage({
-        msg: "something_completed", 
-        data: {
-            subject: "Loading",
-            content: "Just completed!"
-        }
-    });
-      // sendResponse({ type: 'c-to-s', data: 'Response from Content Script to Service Worker' });
-      // sendResponse({ type: 'c-to-s', data: 'Response from Content Script to Service Worker' });
+      // Extract text content from the div element
+      let time = detailsElement.querySelector( 'div#time' ).textContent;
+
+      // Extract text content from the div element
+      let url = detailsElement.querySelector( 'a' ).href;
+
+      output.push( { text: title, time: time, url: url } )
     }
-  });
+    else {
+      console.log( 'Details element not found.' );
+    }
+  }
+  console.log(output)
+  return output
+}
+
+const buildMarkdownChapters = () => {
+  let title = document.title.replace(' - YouTube', '') 
+  let output = `# ${title}
+  
+Chapters:
+`
+  let arr = chaptersArray()
+  let count = arr.length
+
+
+  if (count > 0) {
+    for( let chapter of arr){
+      let template = `- [${chapter['time']}](${chapter['url']}) ${chapter['text']}\n`
+      output = output + template
+    }
+  }
+  return output
+
+}
+
+
+// It will recieve a message from background.js
+chrome.runtime.onMessage.addListener( function ( message, sender, sendResponse ) {
+  if ( message.type === 'background-to-content' ) {
+    // console.log('s-to-c: Received message in Content Script:', message.data);
+    // Handle the message from Service Worker if needed
+    // If a response is expected, you can send it asynchronously
+    chrome.runtime.sendMessage( {
+      type: "content-to-popup",
+      data: {
+        subject: "Loading",
+        content: buildMarkdownChapters()
+      }
+    } );
+  }
+} );
+
